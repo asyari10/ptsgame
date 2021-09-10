3cf500e51370609194d3c12174f19aba4cc2e259
asyari10 <rasyari42@gmail.com>
2021-09-11 01:17:34 +0700

    pts game

---
+++ PTS/Assets/Scenes/spinner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinner : MonoBehaviour
+{
+    public float staSpeed;
+    public float endSpeed;
+    public bool isSpinning = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpinning == true)
+        {
+            transform.Rotate(0, 0, staSpeed, Space.World);
+            staSpeed -= endSpeed;
+        }
+
+        if (staSpeed <= 0)
+        {
+            staSpeed = 0;
+            isSpinning = false;
+        }
+    }
+
+    public void SpinWheel()
+    {
+        staSpeed = UnityEngine.Random.Range(5.000f, 9.000f);
+        endSpeed = UnityEngine.Random.Range(0.003f, 0.009f);
+        isSpinning = true;
+    }
+}
